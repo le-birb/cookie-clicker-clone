8024a577db2e78fccf1427d164e90a4fefc137da
Ben Hansen <TheLordOfAllMuffins@users.noreply.github.com>
2016-11-18 13:22:20 -0800

    Created a module for murder_chlidren

---
+++ murder_children.lua
@@ -0,0 +1,16 @@
+local composer = require "composer" 
+local widget = require "widget"
+
+--this is the module, and is returned
+local murder_children = {}
+
+--this stores different child types, and doesn't need to be visible to other files
+local child_types = {}
+
+child_types.basic = {"health" = 10, "speed" = 1, "soul_reward" = 1}
+
+function murder_children.spawn(type)
+  
+end
+
+return murder_chlidren
